94715d6706a136db0fbf98d7c14abced8cdfd2b6
Samantha Barros <samantha.barros95@gmail.com>
2020-03-31 01:02:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,11 @@ class School
   school.add_student("Zach Morris", 9)  
   school.rooster
   
-  
+  hash["new_key"] = [ ]
+hash["new_key"] << "new_value_for_value_array"
+ 
+hash
+ => {"new_key"=>["new_value_for_value_array"]} 
   
   
   
